f857f78da811b5ee338e7570e69b20ea50d47823
orihay23 <orihay23@gmail.com>
2019-01-19 16:03:22 -0500

    reads receipts from csv and writes cover letters

---
+++ index.js
@@ -0,0 +1,24 @@
+var utils = require('./utils');
+const path = require('path');
+const csvFilePath = path.resolve(__dirname, 'contacts2018.csv');
+
+async function run() {
+    const contacts = await utils.read(csvFilePath);
+    const promises = [];
+    contacts.forEach(async (contact) => {
+        if (parseFloat(contact.amount) >= 75) {
+            const doc = await utils.loadTemplate(path.resolve(__dirname, '2018 Giving Receipts.docx'));
+            doc.setData({
+                name: contact.name,
+            });
+            await utils.writeDoc(doc, contact.name);
+        }
+    });
+}
+
+run();
+
+
+//set the templateVariables
+
+
---
+++ utils.js
@@ -0,0 +1,50 @@
+const csv = require('csvtojson');
+const fs = require('fs');
+const path = require('path');
+const JSZip = require('jszip');
+const Docxtemplater = require('docxtemplater');
+
+async function read(csvFilePath) {
+    return await csv().fromFile(csvFilePath);
+}
+
+async function loadTemplate(docPath) {
+    var content = fs
+    .readFileSync(docPath, 'binary');
+    var zip = new JSZip(content);
+
+    var doc = new Docxtemplater();
+    doc.loadZip(zip);
+    return doc;
+}
+
+async function writeDoc(doc, name) {
+    try {
+        // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+        doc.render()
+    }
+    catch (error) {
+        var e = {
+            message: error.message,
+            name: error.name,
+            stack: error.stack,
+            properties: error.properties,
+        }
+        console.log(JSON.stringify({error: e}));
+        // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
+        throw error;
+    }
+    
+    var buf = doc.getZip()
+                 .generate({type: 'nodebuffer'});
+    
+    // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
+    // console.log(path.resolve(__dirname, 'out', `GLC_Contribution_Receipt_2019_${name}.docx`));
+    fs.writeFileSync(path.resolve(__dirname, 'out', `GLC_Contribution_Receipt_2019_${name}.docx`), buf);
+}
+
+module.exports = {
+    loadTemplate,
+    read,
+    writeDoc,
+}
