2e7c33e64d736aa7f5485cf09581a68029412818
Stephen Kim <stephen@Stephens-MacBook-Pro.local>
2019-01-20 15:59:38 -0500

    new folder write

--- utils.js
+++ utils.js
@@ -34,10 +34,10 @@ async function writeDoc(doc, name) {
         // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
         throw error;
     }
-    
+
     var buf = doc.getZip()
                  .generate({type: 'nodebuffer'});
-    
+
     // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
     fs.writeFileSync(path.resolve(__dirname, 'out', `GLC_Contribution_Receipt_2019_${name}.docx`), buf);
 }
@@ -58,15 +58,15 @@ async function writeDocPg2(doc, name) {
         // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
         throw error;
     }
-    
+
     var buf = doc.getZip()
                  .generate({type: 'nodebuffer'});
-    
+
     // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
     // console.log(path.resolve(__dirname, 'out', `GLC_Contribution_Receipt_2019_${name}.docx`));
-    fs.writeFileSync(path.resolve(__dirname, 'out', `GLC_Contribution_Receipt_2019_pg2_${name}.docx`), buf);
+    fs.writeFileSync(path.resolve(__dirname, 'out/part2', `GLC_Contribution_Receipt_2019_pg2_${name}.docx`), buf);
 }
- 
+
 module.exports = {
     loadTemplate,
     read,
